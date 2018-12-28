fe9ad6eea65bca0ae3067b599388412a9f5da4f7
lumegliola <lumegliola93@hotmail.it>
2018-12-28 20:06:21 +0100

    aggiunta DAOfactory

---
+++ src/dao/DAOFactory.java
@@ -0,0 +1,30 @@
+package dao;
+
+import dao.interfaces.*;
+import dao.implementation.*;
+
+public class DAOFactory {
+		
+	private DAOFactory() {}
+	
+		public static AulaDAO getAulaDAO() {
+			return new AulaDAOimpl();
+		}
+		
+		public static GdSDAO getGdSDAO() {
+			return new GdSDAOimpl();
+		}
+
+		public static UserDAO getUserDAO() {
+			return new UserDAOimpl();
+		}
+	
+		public static CredenzialiDAO getCredenzialiDAO() {
+			return new CredenzialiDAOimpl();
+		}
+	
+		public static OrarioDAO getOrarioDAO() {
+			return new OrarioDAOimpl();
+		}
+
+}
