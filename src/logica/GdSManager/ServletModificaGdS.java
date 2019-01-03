79478a8b4d139c3f34303112f5ad070abc8d1166
lumegliola <lumegliola93@hotmail.it>
2019-01-03 12:37:59 +0100

    it works!

--- src/logica/GdSManager/ServletModificaGdS.java
+++ src/logica/GdSManager/ServletModificaGdS.java
@@ -58,10 +58,10 @@ public class ServletModificaGdS extends HttpServlet {
 						}
 						 String matricolaCretore=gds.getCreatore().getMatricola();
 	                     String matricola=(String)session.getAttribute("matricola");
-                        System.out.println("matricola"+matricola +"& matricola"+matricolaCretore );
+                        System.out.println("matricola"+matricola +" & matricola"+matricolaCretore );
 						if(matricola.equals(matricolaCretore)) { // se la matricola di chi sta modificando è uguale a creatore elimina
 							System.out.println("Gruppo di Studio viene modificato dal creatore!");
-                            DAOFactory.getGdSDAO().doSaveOrUpdate(gds, "", "capocchia", "F8", new Timestamp (2018,10,23, 15,00,00,00),new Timestamp (2018,10,23, 16,00,00, 00));
+                            DAOFactory.getGdSDAO().doSaveOrUpdate(gds, "", "capocchia", "F8", new Timestamp(118,10,23, 15,0,0,0),new Timestamp (118,10,23, 16,0,0,0));
 							
 							session.setAttribute("esito","ok");
 						    getServletContext().getRequestDispatcher("/view/ProvaOutput.jsp").forward(request, response);
--- src/logica/GdSManager/ServletRicercaGds.java
+++ src/logica/GdSManager/ServletRicercaGds.java
@@ -1,41 +1,49 @@
 package logica.GdSManager;
 
+import java.util.List;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class RicercaGds
- */
+import dao.DAOFactory;
+import bean.GruppoDiStudio;
+
 @WebServlet("/RicercaGds")
 public class ServletRicercaGds extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ServletRicercaGds() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		HttpSession session = request.getSession(false);
+		if(session != null && session.getAttribute("logged") != null) {
+			
+			String nomeGruppo = "";
+			String materia = "";
+			
+			List<GruppoDiStudio> elenco = new ArrayList<>();
+			elenco = DAOFactory.getGdSDAO().doRetrieveByNameAndSubject(nomeGruppo, materia);
+			
+			
+		}
+		
+	
 	}
 
 }
