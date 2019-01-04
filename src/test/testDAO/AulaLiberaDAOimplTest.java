50953ad11f3b54575db295feb4578c9873a04491
lumegliola <lumegliola93@hotmail.it>
2019-01-04 10:59:23 +0100

    aulaLiberaDAOtest

--- src/test/testDAO/AulaLiberaDAOimplTest.java
+++ src/test/testDAO/AulaLiberaDAOimplTest.java
@@ -2,13 +2,39 @@ package test.testDAO;
 
 import static org.junit.Assert.*;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import bean.*;
+import bean.AulaLibera;
+import dao.DAOFactory;
+import dao.interfaces.AulaLiberaDAO;
 
 public class AulaLiberaDAOimplTest {
 
+	AulaLiberaDAO a = DAOFactory.getAulaLiberaDAO();
+	Aula aula = new Aula("P4", "F2");
+	Timestamp inizio = new Timestamp(119, 0, 15, 0, 0, 0, 0);
+	Timestamp fine = new Timestamp(119, 0, 15, 0, 100, 0, 0);
+	Orario or = new Orario(inizio, fine);
+	AulaLibera al = new AulaLibera(aula, or);
+	boolean ok = false;
+
 	@Test
 	public void testDoSave() {
-		fail("Not yet implemented");
+		Boolean res = a.doSave(al);
+		assertTrue(res);
+		List<AulaLibera> aRes = a.doRetrieveByName(al.getAula().getNomeAula());
+		for( AulaLibera b : aRes) {
+			if(al.equals(b))
+				ok = true;
+		}
+		assertTrue(ok);
+		
 	}
 
 	@Test
