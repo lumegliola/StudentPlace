fd8099445170936d9f5ee6017fd220cc59e861bf
lumegliola <lumegliola93@hotmail.it>
2019-01-04 09:51:05 +0100

    inizio testing

---
+++ src/dao/implementation/AulaDAOimplTest.java
@@ -0,0 +1,39 @@
+package dao.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AulaDAOimplTest {
+
+	@Test
+	public void testDoSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoSaveOrUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoDeleteAula() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoDeleteString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoRetrieveByKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoRetrieveAll() {
+		fail("Not yet implemented");
+	}
+
+}
