3826e9ec0ae9abebc61f1e497f71cfc4340a2025
Notebook <Notebook@Notebook>
2019-01-06 22:23:27 +0100

    popolazione database

--- ScriptDB/inserimento tabelle.sql
+++ ScriptDB/inserimento tabelle.sql
@@ -5,7 +5,10 @@ insert into credenziali(matricola,email,password,amministratore) values
 ("0512103593", "a.capodanno5@studenti.unisa.it",123456,1),
 ("0512103647", "a.panico19@studenti.unisa.it",123456,1),
 ("0512102855", "f.megliola@studenti.unisa.it",123456,1),
-("0512102865", "f.megliola1@studenti.unisa.it",123456,0);
+("0512102865", "f.megliola1@studenti.unisa.it",123456,0),
+("0512102565", "a.lino@studenti.unisa.it",123456,0),
+("0512102765", "c.iao@studenti.unisa.it",123456,0),
+("0512102665", "b.ello@studenti.unisa.it",123456,0);
 
 
 insert into amministratore(matricola, nome,cognome) values
@@ -17,7 +20,16 @@ insert into amministratore(matricola, nome,cognome) values
 
 insert into studente(matricola, nome,cognome) values
 
-("0512102865", "Filippo","Megliola");
+("0512102865", "Filippo","Megliola"),
+("0512102565", "Antonio","Lino"),
+("0512102765", "Carmine","Iao"),
+("0512102665", "Bautista","Ello"),
+("0512101769", "Luigi","Califano"),
+("0512103853", "Alessia","Crispo"),
+("0512103593", "Alessandro","Capodanno"),
+("0512103647", "Antonio","Panico"),
+("0512102855", "Filippo","Megliola");
+
 
 insert into aula(nome,edificio) values
 
@@ -72,5 +84,24 @@ insert into giorno (giorno)value
 
 
 
+
+
+insert into libera (aula,giorno,orario)value
+("P1",,"lunedi","non so che mettere come orario");
+
+
+
+
+
+
 insert into gds(nome,creatore,materia,oraInizio,oraFine,giorno,aula) values
-("Gruppo di is","0512102865","Ingegneria Del software","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"F1");
+("Gruppo di is","0512102865","Ingegneria Del software","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"F1"),
+
+("Gruppo di pd","0512103647","Programmazione Distribuita","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"F8"),
+
+("Gruppo di ium","0512103593","Interazione Uomo Macchina","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"P1"),
+
+("Gruppo di so","0512103593","Sistemi Operativi","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"P12"),
+
+("Gruppo di analisi","0512102865","Analisi Matematica","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"F2"),
+("Gruppo di psd","0512102865","Programmazione e strutture di dati","2018-12-21, 15:00:00", "2018-12-21, 17:30:00","lunedì" ,"P4");
