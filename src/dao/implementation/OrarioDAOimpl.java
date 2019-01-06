92da1cc22d9c5888b9afc4734edd8290b70ad41f
A.CAPODANNO5 <A.CAPODANNO5@DESKTOP-IREEAHJ>
2019-01-06 19:14:43 +0100

    qualcosa

--- src/dao/implementation/OrarioDAOimpl.java
+++ src/dao/implementation/OrarioDAOimpl.java
@@ -61,19 +61,14 @@ public class OrarioDAOimpl implements OrarioDAO {
 		try {
 			//dichiara lo statement
 			connection = DriverManagerConnectionPool.getConnection();
-			ps = connection.prepareStatement("update orario set inizio = ?, fine = ? where id ="+ DAOFactory.getOrarioDAO().doRetrieveByStartAndFinish(start, end).getIdOrario()+" ;");
+			ps = connection.prepareStatement("update orario set inizio = ?, fine = ? where id =? ;");
 			
 			//CONTROLLA CAMPI DA NON MODIFICARE
 			
-			if(start != null)
-				ps.setTimestamp(1, start);
-			else
-				ps.setTimestamp(1, or.getInizio());
-			
-			if(end != null)
-				ps.setTimestamp(2, end);
-			else
-				ps.setTimestamp(2, or.getInizio());
+			ps.setTimestamp(1, start);
+			ps.setTimestamp(2, end);
+	       	int id= DAOFactory.getOrarioDAO().doRetrieveByStartAndFinish(or.getInizio(),or.getFine()).getIdOrario();
+			ps.setInt(3, or.getIdOrario());
 
 			//esegue lo statement
 			result = ps.executeUpdate();
--- src/test/testDAO/OrarioDAOimplTest.java
+++ src/test/testDAO/OrarioDAOimplTest.java
@@ -1,19 +1,49 @@
 package test.testDAO;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Timestamp;
+
 import org.junit.jupiter.api.Test;
+
+import bean.Credenziali;
+import bean.Orario;
+import dao.DAOFactory;
+import dao.interfaces.CredenzialiDAO;
+import dao.interfaces.OrarioDAO;
 
 class OrarioDAOimplTest {
 
 	@Test
 	void testDoSave() {
-		fail("Not yet implemented");
+		
+		System.out.println("Start test");
+	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+	    assertNotNull(orarioDao);
+	    Orario orario=new Orario();
+	    orario.setInizio(new Timestamp(2019,11,2,12,0,0,0));
+	    orario.setFine(new Timestamp(2019,11,2,14,0,0,0));
+	    boolean valore=orarioDao.doSave(orario);
+	    assertTrue(valore);
+	    System.out.println("End test");
 	}
 
 	@Test
 	void testDoSaveOrUpdate() {
-		fail("Not yet implemented");
+		System.out.println("Start test");
+	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+	    assertNotNull(orarioDao);
+	    Orario orario=new Orario();
+	    orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
+	    orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
+	    orario.setIdOrario(1);
+	    
+	    boolean valore=orarioDao.doSaveOrUpdate(orario, new Timestamp(2019,11,02, 12,00,00, 0), new Timestamp(2019,11,02 ,14,00,00, 0));
+	    assertTrue(valore);
+
+
 	}
 
 	@Test
