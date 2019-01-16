6e5ee21f5c0410a82c323e4ff5057a9f06019763
Phil <Phil@172.19.17.253>
2019-01-16 17:13:04 +0100

    correzioni varie

--- src/test/testDAO/IscrizioneDAOImplTest.java
+++ src/test/testDAO/IscrizioneDAOImplTest.java
@@ -2,153 +2,139 @@ package test.testDAO;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.List;
 
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.operation.DatabaseOperation;
 import org.junit.jupiter.api.Test;
+
 
 import bean.GruppoDiStudio;
 import bean.Iscrizione;
 import bean.Utente;
 import dao.DAOFactory;
+
 import dao.interfaces.GdSDAO;
 import dao.interfaces.IscrizioneDAO;
 import dao.interfaces.UserDAO;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.dbunit.DBTestCase;
-import org.dbunit.database.DatabaseDataSourceConnection;
-import org.dbunit.dataset.CachedDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSet;
-import bean.Aula;
-import dao.implementation.AulaDAOimpl;
-import dao.interfaces.AulaDAO;
 
 class IscrizioneDAOImplTest {
 
-	@Test
-	void testDoSave() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    GdSDAO gdsDao=DAOFactory.getGdSDAO();
-	    UserDAO utenteDao=DAOFactory.getUserDAO();
-	    GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
-	    Utente utente=utenteDao.doRetrieveByKey("0512103457");
-	    assertNotNull(gruppo);
-	    assertNotNull(utente);
-	    Iscrizione iscr=new Iscrizione();
-	    iscr.setGruppo(gruppo);
-	    iscr.setIscritto(utente);
-	    assertNotNull(iscr);
-	    boolean valore=iscrDao.doSave(iscr);
-	    assertTrue(valore);
-	    System.out.println("End test");
-	}
+  @Test
+  void testDoSave() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      GdSDAO gdsDao=DAOFactory.getGdSDAO();
+      UserDAO utenteDao=DAOFactory.getUserDAO();
+      GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
+      Utente utente=utenteDao.doRetrieveByKey("0512103457");
+      assertNotNull(gruppo);
+      assertNotNull(utente);
+      Iscrizione iscr=new Iscrizione();
+      iscr.setGruppo(gruppo);
+      iscr.setIscritto(utente);
+      assertNotNull(iscr);
+      boolean valore=iscrDao.doSave(iscr);
+      assertTrue(valore);
+      System.out.println("End test");
+  }
 
-	@Test
-	void testDoDelete() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    GdSDAO gdsDao=DAOFactory.getGdSDAO();
-	    UserDAO utenteDao=DAOFactory.getUserDAO();
-	    GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
-	    Utente utente=utenteDao.doRetrieveByKey("0512103333");
-	    assertNotNull(gruppo);
-	    assertNotNull(utente);
-	    Iscrizione iscr=new Iscrizione();
-	    iscr.setGruppo(gruppo);
-	    iscr.setIscritto(utente);
-	    assertNotNull(iscr);
-	    boolean valore=iscrDao.doDelete(iscr);
-	    assertTrue(valore);
-	    System.out.println("End test");
-	}
+  @Test
+  void testDoDelete() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      GdSDAO gdsDao=DAOFactory.getGdSDAO();
+      UserDAO utenteDao=DAOFactory.getUserDAO();
+      GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
+      Utente utente=utenteDao.doRetrieveByKey("0512103333");
+      assertNotNull(gruppo);
+      assertNotNull(utente);
+      Iscrizione iscr=new Iscrizione();
+      iscr.setGruppo(gruppo);
+      iscr.setIscritto(utente);
+      assertNotNull(iscr);
+      boolean valore=iscrDao.doDelete(iscr);
+      assertTrue(valore);
+      System.out.println("End test");
+  }
 
-	@Test
-	void testDoDeleteByUserAndGroup() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    GdSDAO gdsDao=DAOFactory.getGdSDAO();
-	    UserDAO utenteDao=DAOFactory.getUserDAO();
-	    GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
-	    Utente utente=utenteDao.doRetrieveByKey("0512103336");
-	    assertNotNull(gruppo);
-	    assertNotNull(utente);
-	    boolean valore=iscrDao.doDeleteByUserAndGroup(utente.getMatricola(), gruppo.getId());
-	    assertTrue(valore);
-	    System.out.println("End test");	}
+  @Test
+  void testDoDeleteByUserAndGroup() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      GdSDAO gdsDao=DAOFactory.getGdSDAO();
+      UserDAO utenteDao=DAOFactory.getUserDAO();
+      GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
+      Utente utente=utenteDao.doRetrieveByKey("0512103336");
+      assertNotNull(gruppo);
+      assertNotNull(utente);
+      boolean valore=iscrDao.doDeleteByUserAndGroup(utente.getMatricola(), gruppo.getId());
+      assertTrue(valore);
+      System.out.println("End test");  }
 
-	@Test
-	void testDoRetrieveByUser() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    UserDAO utenteDao=DAOFactory.getUserDAO();
-	    Utente utente=utenteDao.doRetrieveByKey("0512102865");
-	    assertNotNull(utente);
-	   List <Iscrizione> listIscr=null;
-	    listIscr=iscrDao.doRetrieveByUser(utente.getMatricola());
-	    assertNotNull(listIscr);
-	    System.out.println("End test");
-	}
+  @Test
+  void testDoRetrieveByUser() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      UserDAO utenteDao=DAOFactory.getUserDAO();
+      Utente utente=utenteDao.doRetrieveByKey("0512102865");
+      assertNotNull(utente);
+     List <Iscrizione> listIscr=null;
+      listIscr=iscrDao.doRetrieveByUser(utente.getMatricola());
+      assertNotNull(listIscr);
+      System.out.println("End test");
+  }
 
-	@Test
-	void testDoRetrieveByGroup() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    GdSDAO gdsDao=DAOFactory.getGdSDAO();
-	    GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
-	   
-	    assertNotNull(gruppo);
-	    List <Iscrizione> listIscr=null;
-	    listIscr=iscrDao.doRetrieveByGroup(gruppo.getId());
-	    assertNotNull(listIscr);
-	    System.out.println("End test");	
-	    
-	}
+  @Test
+  void testDoRetrieveByGroup() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      GdSDAO gdsDao=DAOFactory.getGdSDAO();
+      GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
+     
+      assertNotNull(gruppo);
+      List <Iscrizione> listIscr=null;
+      listIscr=iscrDao.doRetrieveByGroup(gruppo.getId());
+      assertNotNull(listIscr);
+      System.out.println("End test");  
+      
+  }
 
-	@Test
-	void testDoRetrieveByUserAndGroup() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    
-	    UserDAO utenteDao=DAOFactory.getUserDAO();
-	    Utente utente=utenteDao.doRetrieveByKey("0512102865");
-	    assertNotNull(utente);
-	    GdSDAO gdsDao=DAOFactory.getGdSDAO();
-	    GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
-	     assertNotNull(gruppo);
-	   
-	    Iscrizione iscr=null;
-	    iscr=iscrDao.doRetrieveByUserAndGroup(utente.getMatricola(),gruppo.getId());
-	    assertNotNull(iscr);
-	    System.out.println("End test");
-	   }
 
-	@Test
-	void testDoRetrieveAll() {
-		System.out.println("Start test");
-	    IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
-	    assertNotNull(iscrDao);
-	    List <Iscrizione> listIscr=null;
-		listIscr=iscrDao.doRetrieveAll();
-		assertNotNull(listIscr);
-	    System.out.println("End test");
-	   
-	}
+@Test
+  void testDoRetrieveByUserAndGroup() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      
+      UserDAO utenteDao=DAOFactory.getUserDAO();
+      Utente utente=utenteDao.doRetrieveByKey("0512102865");
+      assertNotNull(utente);
+      GdSDAO gdsDao=DAOFactory.getGdSDAO();
+      GruppoDiStudio gruppo=gdsDao.doRetrieveByNameAndSubject("Gruppo di is", "Ingegneria Del software");
+       assertNotNull(gruppo);
+     
+      Iscrizione iscr=null;
+      iscr=iscrDao.doRetrieveByUserAndGroup(utente.getMatricola(),gruppo.getId());
+      assertNotNull(iscr);
+      System.out.println("End test");
+     }
+
+  @Test
+  void testDoRetrieveAll() {
+    System.out.println("Start test");
+      IscrizioneDAO iscrDao=DAOFactory.getIscrizioneDAO();
+      assertNotNull(iscrDao);
+      List <Iscrizione> listIscr=null;
+    listIscr=iscrDao.doRetrieveAll();
+    assertNotNull(listIscr);
+      System.out.println("End test");
+     
+  }
 
 }
--- src/test/testDAO/OrarioDAOimplTest.java
+++ src/test/testDAO/OrarioDAOimplTest.java
@@ -4,161 +4,129 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.Timestamp;
 import java.util.Iterator;
 import java.util.List;
 
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.operation.DatabaseOperation;
 import org.junit.jupiter.api.Test;
+
 
 import bean.Orario;
 import dao.DAOFactory;
 import dao.interfaces.OrarioDAO;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.List;
-
-import org.dbunit.DBTestCase;
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.database.DatabaseDataSourceConnection;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.CachedDataSet;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.operation.DatabaseOperation;
-import org.junit.Test;
-
-import bean.Aula;
-import dao.DAOFactory;
-import dao.implementation.AulaDAOimpl;
-import dao.interfaces.AulaDAO;
 
 class OrarioDAOimplTest {
 
-	@Test
-	void testDoSave() {
-		
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    Orario orario=new Orario();
-	    orario.setInizio(new Timestamp(2019,11,2,12,0,0,0));
-	    orario.setFine(new Timestamp(2019,11,2,14,0,0,0));
-	    boolean valore=orarioDao.doSave(orario);
-	    assertTrue(valore);
-	    System.out.println("End test");
-	}
+  @Test
+  void testDoSave() {
+    
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      Orario orario=new Orario();
+      orario.setInizio(new Timestamp(2019,11,2,12,0,0,0));
+      orario.setFine(new Timestamp(2019,11,2,14,0,0,0));
+      boolean valore=orarioDao.doSave(orario);
+      assertTrue(valore);
+      System.out.println("End test");
+  }
 
-	@Test
-	void testDoSaveOrUpdate() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    Orario orario=new Orario();
-	    orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
-	    orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
-	    orario.setIdOrario(1);
-	    
-	    boolean valore=orarioDao.doSaveOrUpdate(orario, new Timestamp(2019,11,03,13,00,00, 0), new Timestamp(2019,11,03 ,15,00,00, 0));
-	    assertTrue(valore);
+  @Test
+  void testDoSaveOrUpdate() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      Orario orario=new Orario();
+      orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
+      orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
+      orario.setIdOrario(1);
+      
+      boolean valore=orarioDao.doSaveOrUpdate(orario, new Timestamp(2019,11,03,13,00,00, 0), new Timestamp(2019,11,03 ,15,00,00, 0));
+      assertTrue(valore);
 
-	    System.out.println("End test");
+      System.out.println("End test");
 
-	}
+  }
 
-	@Test
-	void testDoDeleteOrario() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    Orario orario=new Orario();
-	    orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
-	    orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
-	    orario.setIdOrario(7);
-	    boolean valore=orarioDao.doDelete(orario);
-	    assertTrue(valore);
+  @Test
+  void testDoDeleteOrario() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      Orario orario=new Orario();
+      orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
+      orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
+      orario.setIdOrario(7);
+      boolean valore=orarioDao.doDelete(orario);
+      assertTrue(valore);
 
-	    System.out.println("End test");
-	    }
+      System.out.println("End test");
+      }
 
-	@Test
-	void testDoDeleteInt() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    Orario orario=new Orario();
-	    orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
-	    orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
-	    orario.setIdOrario(8);
-	    boolean valore=orarioDao.doDelete(orario.getIdOrario());
-	    assertTrue(valore);
+  @Test
+  void testDoDeleteInt() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      Orario orario=new Orario();
+      orario.setInizio(new Timestamp(2019,11,02,11,00,00, 0));
+      orario.setFine(new Timestamp(2019,11,02, 13,00,00, 0));
+      orario.setIdOrario(8);
+      boolean valore=orarioDao.doDelete(orario.getIdOrario());
+      assertTrue(valore);
 
-	    System.out.println("End test");	}
+      System.out.println("End test");  }
 
-	@Test
-	void testDoRetrieveByKey() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    Orario orario=null;
-	    orario=orarioDao.doRetrieveByKey(6);
-	    assertNotNull(orario);
-	    System.out.println("End test");
-	    }
+  @Test
+  void testDoRetrieveByKey() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      Orario orario=null;
+      orario=orarioDao.doRetrieveByKey(6);
+      assertNotNull(orario);
+      System.out.println("End test");
+      }
 
-	@Test
-	void testDoRetrieveByStart() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    List<Orario> listOrario=null;
-	    listOrario=orarioDao.doRetrieveByStart(new Timestamp(2019,11,02,11,00,00, 0));
-	    assertNotNull(listOrario);
-	    for (Orario or :listOrario) {
-	   	 System.out.println("Start"+or.getInizio()+"Fine"+or.getFine());
-	   		}
-	    System.out.println("End test");
-	
-	}
+  @Test
+  void testDoRetrieveByStart() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      List<Orario> listOrario=null;
+      listOrario=orarioDao.doRetrieveByStart(new Timestamp(2019,11,02,11,00,00, 0));
+      assertNotNull(listOrario);
+      for (Orario or :listOrario) {
+        System.out.println("Start"+or.getInizio()+"Fine"+or.getFine());
+         }
+      System.out.println("End test");
+  
+  }
 
-	@Test
-	void testDoRetrieveByStartAndFinish() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    Orario orario=null;
-	    orario=orarioDao.doRetrieveByStartAndFinish(new Timestamp(2019,11,02,11,00,00, 0),new Timestamp(2019,11,02,13,00,00, 0));
-	    assertNotNull(orario);
-	   
-	    System.out.println("End test");
-	
-	}
+  @Test
+  void testDoRetrieveByStartAndFinish() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      Orario orario=null;
+      orario=orarioDao.doRetrieveByStartAndFinish(new Timestamp(2019,11,02,11,00,00, 0),new Timestamp(2019,11,02,13,00,00, 0));
+      assertNotNull(orario);
+     
+      System.out.println("End test");
+  
+  }
 
-	@Test
-	void testDoRetrieveAll() {
-		System.out.println("Start test");
-	    OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
-	    assertNotNull(orarioDao);
-	    List<Orario> listOrario=null;
-	    listOrario=orarioDao.doRetrieveAll();
-	    assertNotNull(listOrario);
-	    for (Orario or :listOrario) {
-	   	 System.out.println("Start"+or.getInizio()+"Fine"+or.getFine());
-	   		}
-	    System.out.println("End test");	}
+  @Test
+  void testDoRetrieveAll() {
+    System.out.println("Start test");
+      OrarioDAO orarioDao=DAOFactory.getOrarioDAO();
+      assertNotNull(orarioDao);
+      List<Orario> listOrario=null;
+      listOrario=orarioDao.doRetrieveAll();
+      assertNotNull(listOrario);
+      for (Orario or :listOrario) {
+        System.out.println("Start"+or.getInizio()+"Fine"+or.getFine());
+         }
+      System.out.println("End test");  }
 
 }
-
-
