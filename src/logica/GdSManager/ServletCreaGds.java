ade3c070b425aa6e5e2e2912c3bcdfc46d197e08
A.CAPODANNO5 <A.CAPODANNO5@DESKTOP-IREEAHJ>
2019-01-16 16:49:38 +0100

    okokok

--- src/logica/GdSManager/ServletCreaGds.java
+++ src/logica/GdSManager/ServletCreaGds.java
@@ -39,7 +39,7 @@ public class ServletCreaGds extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
+		HttpSession session = request.getSession(false);						//
 		if(session != null && session.getAttribute("logged") != null) {
 			
 			String nomeGruppo = request.getParameter("nomeGruppo");
