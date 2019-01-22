a24ea05f5f1166aa8b3cc01cc528d9d878d1ab8c
A.CAPODANNO5 <A.CAPODANNO5@DESKTOP-IREEAHJ>
2019-01-22 22:51:08 +0100

    add cose

--- src/logica/orarioManager/ServletModificaOrario.java
+++ src/logica/orarioManager/ServletModificaOrario.java
@@ -1,11 +1,17 @@
 package logica.orarioManager;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Orario;
+import dao.DAOFactory;
 
 /**
  * Servlet implementation class ModificaOrario
@@ -27,7 +33,7 @@ public class ServletModificaOrario extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+			doPost(request, response);
 	}
 
 	/**
@@ -35,7 +41,30 @@ public class ServletModificaOrario extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		HttpSession session = request.getSession(false);
+		//solo l`amministratore può inserire un orario
+		if(session != null && session.getAttribute("admin").equals("admin")){
+			
+			Orario or = new Orario();
+			or.setInizio(Timestamp.valueOf(request.getParameter("inizio")));
+			or.setFine(Timestamp.valueOf(request.getParameter("fine")));
+			
+			//controlla che l`orario non sia presente
+			Orario controllo = DAOFactory.getOrarioDAO().doRetrieveByStartAndFinish(or.getInizio(), or.getFine());
+			if(controllo != null) {
+				System.out.println("Orario già presente!");
+				session.setAttribute("esito", "errore");
+			}
+			else {
+				DAOFactory.getOrarioDAO().doSave(or);
+				System.out.println("Orario inserito dall`amministratore");
+				session.setAttribute("esito", "ok");
+				request.getRequestDispatcher("ProvaOutput.jsp");
+			}
+			
+		}else {
+			session.setAttribute("esito", "errore");
+			request.getRequestDispatcher("ProvaOutput.jsp");
+		}	}
 
 }
