24f0700d459d0dc81e26dc72deedfdbe23887ad0
lumegliola <lumegliola93@hotmail.it>
2019-01-26 20:11:32 +0100

    lavoro appeso(se ne parla dopo cena)

--- src/logica/orarioManager/ServletAulaLibera.java
+++ src/logica/orarioManager/ServletAulaLibera.java
@@ -1,11 +1,18 @@
 package logica.orarioManager;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bean.AulaLibera;
+import bean.Orario;
+import dao.DAOFactory;
 
 /**
  * Servlet implementation class ServletAulaLibera
@@ -34,7 +41,22 @@ public class ServletAulaLibera extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String inizio = request.getParameter()
+		String inizio = request.getParameter("inizio");
+		String fine = request.getParameter("fine");
+		String giorno = request.getParameter("data");
+		Timestamp orIn =  Timestamp.valueOf(giorno+" "+inizio+":00:00");
+		Timestamp orFin =  Timestamp.valueOf(giorno+" "+fine+":00:00");
+		
+		Orario or = DAOFactory.getOrarioDAO().doRetrieveByStartAndFinish(orIn, orFin);
+		
+	
+		
+		
+		if(or!=null) {
+			List<AulaLibera> aule = DAOFactory.getAulaLiberaDAO().doRetrieveByDate(orIn);
+		}
+		
+		
 	}
 
 }
