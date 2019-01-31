a079885cf4f259d82f558fc1e177a7b33daa84e7
lumegliola <lumegliola93@hotmail.it>
2019-01-31 18:01:31 +0100

    iscrizione a gds

---
+++ src/testSistema/IscrizioneGdS.java
@@ -0,0 +1,54 @@
+package testSistema;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class IscrizioneGdS {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://www.katalon.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testIscrizioneGdS() throws Exception {
+		driver.get("http://localhost:8080/StudentPlace/ShowHome");
+		driver.findElement(By.id("email")).click();
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("a.lino@studenti.unisa.it");
+		driver.findElement(By.id("password")).click();
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Registrati'])[1]/preceding::input[1]")).click();
+		driver.findElement(By.id("search_input")).click();
+		driver.findElement(By.id("search_input")).clear();
+		driver.findElement(By.id("search_input")).sendKeys("ana");
+		driver.findElement(By.id("search_input")).sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gruppo di analisi'])[1]/following::td[1]")).click();
+		driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='alle:'])[1]/following::button[1]")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+
+}
