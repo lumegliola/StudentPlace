d06146da840c078ac4451079f343a0b947eb5565
Aless <Aless@172.19.20.4>
2019-02-07 12:50:16 +0100

    Checkstyle dao factory

--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -4,13 +4,13 @@ import model.dao.implementation.*;
 import model.dao.interfaces.*;
 
 public class DAOFactory {
-		
-	private DAOFactory() {}
-	
+
+        private DAOFactory() { }
+
 		public static AulaDAO getAulaDAO() {
 			return new AulaDAOimpl();
 		}
-		
+
 		public static GdSDAO getGdSDAO() {
 			return new GdSDAOimpl();
         }
@@ -18,15 +18,15 @@ public class DAOFactory {
 		public static UserDAO getUserDAO() {
 			return new UserDAOimpl();
 		}
-	
+
 		public static OrarioDAO getOrarioDAO() {
 			return new OrarioDAOimpl();
 		}
-		
+
 		public static AulaLiberaDAO getAulaLiberaDAO() {
 			return new AulaLiberaDAOimpl();
 		}
-		
+
 		public static IscrizioneDAO getIscrizioneDAO() {
 			return new IscrizioneDAOImpl();
 		}
