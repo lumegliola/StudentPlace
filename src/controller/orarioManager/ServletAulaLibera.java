875dc7dcace9855847749b7dfb551f8b58f3099e
Aless <Aless@172.19.20.4>
2019-02-07 12:51:23 +0100

    Checkstyle aula libera e elimina orario

--- src/controller/orarioManager/ServletAulaLibera.java
+++ src/controller/orarioManager/ServletAulaLibera.java
@@ -29,19 +29,19 @@ import model.dao.DAOFactory;
 @WebServlet("/AulaLibera")
 public class ServletAulaLibera extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletAulaLibera() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
+	/*
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletAulaLibera() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doPost(request, response);
 	}
@@ -49,72 +49,66 @@ public class ServletAulaLibera extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@SuppressWarnings("deprecation")
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("dvr");
-	String inizio = request.getParameter("inizio");
-	String fine = request.getParameter("fine");
+		String inizio = request.getParameter("inizio");
+		String fine = request.getParameter("fine");
 
-	String giorno = request.getParameter("data");
-	String data=giorno.concat(" "+inizio.concat(":00:00"));
-    SimpleDateFormat sdf;
-    sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    Date date2=new Date(),date1=new Date();	 
-    try {
-	 date1=sdf.parse(data);	
-	 date2=sdf.parse(data);
-	} catch (ParseException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}	
-    	Timestamp orIn = new  Timestamp(date1.getTime());
-    	Timestamp orFine = new  Timestamp(date2.getTime());
-			System.out.println(data);
-		
-	      List<AulaLibera> naule = DAOFactory.getAulaLiberaDAO().doRetrieveAll();
-	      List<AulaLibera> aule = new ArrayList<AulaLibera>();
+		String giorno = request.getParameter("data");
+		String data = giorno.concat(" " + inizio.concat(":00:00"));
+		SimpleDateFormat sdf;
+		sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date2 = new Date();
+		Date date1 = new Date();
+		try {
+			date1 = sdf.parse(data);
+			date2 = sdf.parse(data);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-              for (int i=0;i<naule.size();i++) {
-            	  
-        		  
-            	  if(naule.get(i).getOrario().getInizio().getYear() == orIn.getYear() &&
-            			  naule.get(i).getOrario().getInizio().getMonth() == orIn.getMonth()&&
-            			  naule.get(i).getOrario().getInizio().getDate() == orIn.getDate()&&
-            			  naule.get(i).getOrario().getInizio().getHours() <= orIn.getHours()&&
-            			  naule.get(i).getOrario().getFine().getHours() >= orFine.getHours()) {
-            		
-            		  System.out.println(naule.get(i).getOrario().getInizio()+"|"+naule.get(i).getOrario().getFine());
-            		  aule.add(naule.get(i));
-            		  
-            	  }
-            	  
-            	  
-            	  
-            	  
-            	  
-            	  
-              }		    
-			 //ho ricavato una lista di aule libere a partire dall'orario di inserimento fino alla fine della gioranta scelta
-	
-         	PrintWriter out = response.getWriter();
-	        response.setContentType("application/json");
-	        response.setCharacterEncoding("UTF-8");
-	        out.append("[");	        
-	        for(int i=0;i<aule.size();i++) {
-	        	out.append("{"
-	        			+ "\"aula\":\""+aule.get(i).getAula().getNomeAula()+"\","
-	        			+ "\"giorno\":\""+aule.get(i).getGiorno()+"\","
-	        			+ "\"id\":\""+i+"\","
-    					+ "\"inizio\":\""+(orIn.getHours())+1+"\","
-    				    + "\"fine\":\""+(orFine.getHours()-1)+"\""
-    					+ "}");
-	        	if(i<aule.size()-1) {
-	        		out.append(",");
-	        	}
-	        }
-	        out.append("]");
-	        //out.flush();
-	        //out.close();
+		Timestamp orIn = new  Timestamp(date1.getTime());
+		Timestamp orFine = new  Timestamp(date2.getTime());
+		System.out.println(data);
+
+		List<AulaLibera> naule = DAOFactory.getAulaLiberaDAO().doRetrieveAll();
+		List<AulaLibera> aule = new ArrayList<AulaLibera>();
+
+		for (int i = 0; i < naule.size(); i++) {
+			if (naule.get(i).getOrario().getInizio().getYear() == orIn.getYear()
+					&& naule.get(i).getOrario().getInizio().getMonth() == orIn.getMonth()
+					&& naule.get(i).getOrario().getInizio().getDate() == orIn.getDate()
+					&& naule.get(i).getOrario().getInizio().getHours() <= orIn.getHours()
+					&& naule.get(i).getOrario().getFine().getHours() >= orFine.getHours()) {
+
+				System.out.println(naule.get(i).getOrario().getInizio() + "|" + naule.get(i).getOrario().getFine());
+				aule.add(naule.get(i));
+			}
+		}     //ho ricavato una lista di aule libere a partire dall'orario di inserimento fino alla fine della gioranta scelta
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		out.append("[");
+		for (int i = 0; i < aule.size(); i++) {
+			out.append("{" + "\"aula\":\"" + aule.get(i).getAula().getNomeAula()
+					+ "\"," + "\"giorno\":\""
+					+ aule.get(i).getGiorno()
+					+ "\"," + "\"id\":\"" + i
+					+ "\"," + "\"inizio\":\""
+					+ (orIn.getHours()) + 1
+					+ "\"," + "\"fine\":\"" + (orFine.getHours() - 1)
+					+ "\"" + "}");
+			if (i < aule.size() - 1) {
+				out.append(",");
+			}
+		}
+		out.append("]");
+		//out.flush();
+		//out.close();
 	}
-	
+
 
 }
--- src/controller/orarioManager/ServletEliminaOrario.java
+++ src/controller/orarioManager/ServletEliminaOrario.java
@@ -29,34 +29,33 @@ public class ServletEliminaOrario extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doPost(request,response);
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
 		// solo l'amministratore può cancellare un orario
-		if(session.getAttribute("logged").equals(true)  && session.getAttribute("admin").equals((true) )) {
+		if (session.getAttribute("logged").equals(true)  && session.getAttribute("admin").equals((true))) {
 			System.out.println("inizio if");
 			int idOrario = Integer.parseInt(request.getParameter("idOrario"));
 			Orario or = DAOFactory.getOrarioDAO().doRetrieveByKey(idOrario);
 			//orario non trovato
 			if (or == null) {
 				request.getRequestDispatcher("showGestioneOrario").forward(request, response);
-			}
-			else {
+            } else {
 				DAOFactory.getAulaLiberaDAO().doDeleteByOrario(or.getIdOrario());
 				DAOFactory.getOrarioDAO().doDelete(or);
 				System.out.println("l`orario viene eliminato dall`amministratore");
 				session.setAttribute("esito", "ok");
 				request.getRequestDispatcher("/GestioneOrario").forward(request, response);
 			}
-		}else {
+		} else {
 			request.getRequestDispatcher("view/errore/Errore.jsp").forward(request, response);
 		}
 	}
