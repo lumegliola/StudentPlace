0bbe5f778a8ddd016beec310223759185944f185
lumegliola <lumegliola93@hotmail.it>
2019-02-07 10:07:47 +0100

    aggiunto script fallimentare

--- testSistema/testSistema/CreazioneGdS.java
+++ testSistema/testSistema/CreazioneGdS.java
@@ -1,13 +1,20 @@
 package testSistema;
 
-import java.util.regex.Pattern;
+import static org.junit.Assert.fail;
+
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait; 
+
 
 public class CreazioneGdS {
 
@@ -16,12 +23,47 @@ public class CreazioneGdS {
 	  private boolean acceptNextAlert = true;
 	  private StringBuffer verificationErrors = new StringBuffer();
 
+	 
+ 
+	  
+	  public boolean waitForJQueryToLoad() {
+
+		    WebDriverWait wait = new WebDriverWait(driver, 30);
+
+		    // wait for jQuery to load
+		    ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+		      @Override
+		      public Boolean apply(WebDriver driver) {
+		        try {
+		          return ((Long)((JavascriptExecutor)driver).executeScript("return jQuery.active") == 0);
+		        }
+		        catch (Exception e) {
+		          // no jQuery present
+		          return true;
+		        }
+		      }
+		    };
+
+		    // wait for Javascript to load
+		    ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+		      @Override
+		      public Boolean apply(WebDriver driver) {
+		        return ((JavascriptExecutor)driver).executeScript("return document.readyState")
+		        .toString().equals("complete");
+		      }
+		    };
+
+		  return wait.until(jQueryLoad) && wait.until(jsLoad);
+		}
+	  
+	  
+	  
+	  
 	  @Before
 	  public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
-	    baseUrl = "https://www.katalon.com/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	  }
 
 	  @Test
@@ -45,10 +87,15 @@ public class CreazioneGdS {
 	    driver.findElement(By.id("inizio")).click();
 	    driver.findElement(By.id("inizio")).clear();
 	    driver.findElement(By.id("inizio")).sendKeys("09:00");
+	    
+	    
+	    waitForJQueryToLoad();
 	    driver.findElement(By.id("aule")).click();
 	    new Select(driver.findElement(By.id("aule"))).selectByVisibleText("P3");
 	    driver.findElement(By.id("aule")).click();
 	    driver.findElement(By.id("bottone")).click();
+	    
+	    
 	  }
 
 	  @After
