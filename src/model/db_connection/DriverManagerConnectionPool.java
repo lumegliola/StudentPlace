3d4ad88f3e5e076c0b3ff0b19a8214dc4d40a5f9
lumegliola <lumegliola93@hotmail.it>
2019-02-08 19:31:40 +0100

    piccole correzioni

--- src/model/db_connection/DriverManagerConnectionPool.java
+++ src/model/db_connection/DriverManagerConnectionPool.java
@@ -6,20 +6,39 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import model.bean.AulaLibera;
+
+/**
+*
+* DriverManagerConnectionPool.java
+* Gestisce la connessione dell'applicazione con il database
+*
+* @author F. Megliola & A. Capodanno
+* @since 12-16-2018
+*
+*
+* */
+
+
 public class DriverManagerConnectionPool  {
 
-	private static List<Connection> freeDbConnections;    //lista di connessioni
+	private static List<Connection> freeDbConnections;    
 
 	static {
-		freeDbConnections = new LinkedList<Connection>();				//il caricamento del driver deve avvenire prima che il collegamento sia stabilito
+		freeDbConnections = new LinkedList<Connection>();				
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");						//		in questo momento passiamo il nome del driver
+			Class.forName("com.mysql.cj.jdbc.Driver");						
 		} catch (ClassNotFoundException e) {
 			System.out.println("DB driver not found:"+ e.getMessage());
 }
 	}
 
-	private static synchronized Connection createDBConnection() throws SQLException {				//passiamo l'url contenetne il nome del db, Utente , password e parametri vari
+	/**
+	 * Crea una connessione con il database usando dei parametri preimpostati
+	 * @return  Connection
+	 * @see Connection
+	 * */
+	private static synchronized Connection createDBConnection() throws SQLException {				
 		Connection newConnection = null;
 		String ip = "localhost";
 		String port = "3306";
@@ -29,32 +48,45 @@ public class DriverManagerConnectionPool  {
 
 		newConnection = DriverManager.getConnection("jdbc:mysql://"+ ip + ":" + port + "/" + db + "?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
 
-		newConnection.setAutoCommit(true);			//preserva l'integrità dei dati, inviando un unico pacchetto di informazioni
+		newConnection.setAutoCommit(true);			
 		return newConnection;
 	}
 
+	/**
+	 * Effettua un controllo nella lista di connessioni libere, se questa è vuota,
+	 * ne crea una invocando createDbConnecion()
+	 * @return  Connection
+	 * @see Connection
+	 * */
 	public static synchronized Connection getConnection() throws SQLException {
 		Connection connection;
 
-		if (!freeDbConnections.isEmpty()) {	 									//se la lista di connessioni non è vuota, preleva la prima
+		if (!freeDbConnections.isEmpty()) {	 									
 			connection = (Connection) freeDbConnections.get(0);
 			freeDbConnections.remove(0);
 
 			try {
-				if (connection.isClosed())										//se la connessione e chiusa, la riottiene
+				if (connection.isClosed())										
 					connection = getConnection();
 			} catch (SQLException e) {
 				connection.close();
 				connection = getConnection();
 			}
 		} else {
-			connection = createDBConnection();									//altrimenti crea la connessione al db
+			connection = createDBConnection();									
 		}
 
 		return connection;
 	}
 
+	/**
+	 * Effettua un controllo della connessione passata in input, se questa non è null,
+	 * la rilascia, aggiungendola alla lista di connessioni libere
+	 * @param connection la connessione da chiudere
+	 * @return  Connection
+	 * @see Connection
+	 * */
 	public static synchronized void releaseConnection(Connection connection) throws SQLException {
-		if(connection != null) freeDbConnections.add(connection);									//rimette la connessione in lista, se esiste
+		if(connection != null) freeDbConnections.add(connection);									
 	}
 }
